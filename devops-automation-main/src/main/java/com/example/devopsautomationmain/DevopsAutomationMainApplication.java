49d1e97d4c4472894e02c432426e5a740293c1cd
Omaya97 <omaya.testouri@edu.isetcom.tn>
2023-01-29 19:40:34 +0100

    Initial commit

---
+++ devops-automation-main/src/main/java/com/example/devopsautomationmain/DevopsAutomationMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsautomationmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAutomationMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAutomationMainApplication.class, args);
+	}
+
+}
---
+++ devops-automation-main/src/test/java/com/example/devopsautomationmain/DevopsAutomationMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsautomationmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutomationMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
